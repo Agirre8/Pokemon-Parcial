18416cfba1726e57907b23ddaec1b55ab1cd8591
Agirre8 <inagi30@gmail.com>
2023-04-17 09:11:55 +0200

    delete los archivos de pruevas

--- pruebas.py
+++
@@ -1,7 +0,0 @@
-import random
-def choose_first_turn(pokemon1, pokemon2):
-  lista = [pokemon1, pokemon2]
-  starter_pokemon = random.choice(lista)
-  print(starter_pokemon)
-
-  choose_first_turn("1", "2")
